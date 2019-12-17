e83b86ed34a060d7792ebd2c88d43b615c8f3bf2
Ciaran Lawlor <ciaran.lawlor@onfido.com>
2019-12-17 17:23:55 +0000

    Intial Setup (#1)
    * Setup initial TypeScript project
    
    * Add Travis CI config
    
    * Test on multiple node versions

---
+++ rollup.config.ts
@@ -0,0 +1,18 @@
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+      sourcemap: true
+    },
+    {
+      file: "dist/index.es.js",
+      format: "es",
+      sourcemap: true
+    }
+  ],
+  plugins: [typescript()]
+};
