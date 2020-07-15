12612db66dbacddb344c04ad4da12d7f73705bee
Kacper Kula <kulak@kulak.at>
2020-07-15 10:34:52 +0100

    Adding Autofill endpoint. (#21)
    * Adding Autofill endpoint.
    
    * MR comments adjustments.
    
    * Tests for autofill endpoint.
    
    * Fixes after MR.
    
    * Bumped version, changed test data slightly.

---
+++ test/resources/Autofill.test.ts
@@ -0,0 +1,54 @@
+import nock from "nock";
+import { Onfido } from "onfido-node";
+
+const onfido = new Onfido({ apiToken: "api_token" });
+
+// Fake data, taken from documentation.
+const exampleAutofillJson = {
+  document_id: "21345-xxx",
+  document_classification: {
+    issuing_country: "FRA",
+    document_type: "national_identity_card"
+  },
+  extracted_data: {
+    date_of_birth: "1990-07-21",
+    date_of_expiry: "2025-07-07",
+    document_number: "200407512345",
+    first_name: "AMANDINE CHANTAL",
+    gender: "Female",
+    last_name: "MAVARINE",
+    mrz_line1: "IDFRAMAVARINE<<<<<<<<<<<<<<<<<075123",
+    mrz_line2: "2000000000000AMANDINE<CHANT9007219F5",
+    nationality: "FRA"
+  }
+};
+
+const exampleAutofillResult = {
+  documentId: "21345-xxx",
+  documentClassification: {
+    issuingCountry: "FRA",
+    documentType: "national_identity_card"
+  },
+  extractedData: {
+    dateOfBirth: "1990-07-21",
+    dateOfExpiry: "2025-07-07",
+    documentNumber: "200407512345",
+    firstName: "AMANDINE CHANTAL",
+    gender: "Female",
+    lastName: "MAVARINE",
+    mrzLine1: "IDFRAMAVARINE<<<<<<<<<<<<<<<<<075123",
+    mrzLine2: "2000000000000AMANDINE<CHANT9007219F5",
+    nationality: "FRA"
+  }
+};
+
+it("performs autofill", async () => {
+  nock("https://api.onfido.com/v3")
+    .post("/extractions/", {
+      document_id: "21345-xxx"
+    })
+    .reply(201, exampleAutofillJson);
+
+  const result = await onfido.autofill.perform("21345-xxx");
+  expect(result).toEqual(exampleAutofillResult);
+});
