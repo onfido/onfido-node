ae838df1421e4d9e0168b66e618bfe94f2c18c11
Davide Vacca <51378803+dvacca@users.noreply.github.com>
2022-08-23 14:35:44 +0200

    Improve Applicants tests (#77)
    Allow applicant tests to target effective API instead of just the mock-up one.

--- test/testHelpers.ts
+++ test/testHelpers.ts
@@ -1,7 +1,22 @@
 import nock from "nock";
 import { Onfido, Region } from "onfido-node";
 
-export const onfido = new Onfido({ region: Region.EU, apiToken: "api_token" });
+export const onfido = new Onfido({ region: Region.EU, apiToken: process.env.ONFIDO_API_TOKEN || "api_token" });
 
 export const createNock = (): nock.Scope =>
   nock("https://api.eu.onfido.com/v3.4");
+
+export function getExpectedObject( exampleObject: any ) {
+  // Don't perform any change when replies are mocked up
+  if ( process.env.NOCK_OFF !== 'true' ) {
+    return exampleObject
+  }
+
+  var expectedObject = { ... exampleObject };
+
+  expectedObject.id = expect.stringMatching(/[0-9-]+/);
+  expectedObject.href = expect.stringMatching(/^\/.+/);
+  expectedObject.createdAt = expect.stringMatching(/[0-9TZ:-]+/);
+
+  return expectedObject;
+}
