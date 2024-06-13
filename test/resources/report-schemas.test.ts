cffbf80e47f089d1dfa6774634345de878bde278
Davide Vacca <davide.vacca@onfido.com>
2024-06-13 18:41:31 +0200

    Fix CHANGELOG and improve CI and tests

--- test/resources/report-schemas.test.ts
+++ test/resources/report-schemas.test.ts
@@ -1,4 +1,10 @@
-import { Applicant, Check, Document, Report } from "onfido-node";
+import {
+  Applicant,
+  Check,
+  Document,
+  DocumentReport,
+  Report
+} from "onfido-node";
 import {
   exampleDocumentReport,
   exampleFacialSimilarityPhotoReport
@@ -41,6 +47,14 @@ it("schema of document report should be valid", async () => {
   );
 
   expect(report).toEqual(getExpectedDocumentReport(exampleDocumentReport));
+  expect(report.name).toEqual("document");
+
+  const document_report = report as DocumentReport;
+
+  expect(
+    document_report.breakdown.data_comparison.breakdown.issuing_country.result
+  ).toEqual("clear");
+  expect(document_report.properties.date_of_birth).toEqual("1990-01-01");
 }, 30000);
 
 it("schema of facial similarity photo report should be valid", async () => {
