13f8cd906dae67e70e7919104fbc8bf7df449db2
dvacca-onfido <134616519+dvacca-onfido@users.noreply.github.com>
2024-06-20 12:05:00 +0000

    Upgrade after onfido-openapi-spec change e92d340

---
+++ file-transfer.ts
@@ -0,0 +1,20 @@
+import { readFileSync, PathLike } from "fs";
+
+export class FileTransfer {
+  public readonly buffer: Buffer;
+  public readonly filename: String;
+
+  constructor(buffer: String, filename: String);
+  constructor(buffer: Buffer, filename: String);
+  constructor(inputFile: PathLike);
+
+  constructor(data?: String | Buffer | PathLike, filename?: String) {
+    if (filename == null) {
+      this.buffer = readFileSync(data as PathLike);
+      this.filename = (data as PathLike).toString();
+    } else {
+      this.buffer = data as Buffer;
+      this.filename = filename;
+    }
+  }
+}
--- index.ts
+++ index.ts
@@ -17,3 +17,4 @@ export * from "./api";
 export * from "./configuration";
 
 export * from "./webhook-event-verifier";
+export * from "./file-transfer";
